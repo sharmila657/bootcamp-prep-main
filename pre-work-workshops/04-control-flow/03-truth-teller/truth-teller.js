c32bf466e859c70943b11be1234d8d380008a217
sharmila657 <sharmilajirel8@gmail.com>
2023-06-02 16:27:26 +0545

    truth teller

--- pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
+++ pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
@@ -1,8 +1,24 @@
-let boolean1 = true, boolean2 = true;
-// let boolean1 = true, boolean2 = false;
+// let boolean1 = true, boolean2 = true;
+let boolean1 = true, boolean2 = false;
 // let boolean1 = false, boolean2 = true;
 // let boolean1 = false, boolean2 = false;
 
 let result;
 
 // YOUR CODE BELOW
+
+if (boolean1 && boolean2){
+    result = "both"
+    console.log(result)
+
+}
+
+else if(boolean1 || boolean2){
+    result = "one"
+    console.log(result)
+}
+
+else {
+    result = "none"
+    console.log(result)
+}
