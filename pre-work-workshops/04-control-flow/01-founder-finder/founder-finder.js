3836552085097e165f739d948c856a2919a97b20
sharmila657 <sharmilajirel8@gmail.com>
2023-06-02 15:08:03 +0545

    found finder

--- pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
+++ pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
@@ -1,7 +1,20 @@
-let name = 'David';
+let name = "David";
 // let name = 'Nimit';
 // let name = 'Someone else';
 
-let found;
+let found = true
 
 // YOUR CODE BELOW
+
+if (name === "David"){
+    console.log(found)
+}
+
+else if(name === "Nimit"){
+    console.log(found)
+}
+
+else if(name==="Someone else"){
+console.log("false")
+    
+}
