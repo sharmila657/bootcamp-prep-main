af82428bf7c6d62ee0034dbc493bf3c6ea7c36e5
sharmila657 <sharmilajirel8@gmail.com>
2023-06-05 22:18:35 +0545

    my mnemonic

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,27 @@
 // YOUR CODE BELOW
+
+function myMnemonic(name1, name2, name3, name4){
+    let str = "";
+
+    if (name1){
+        str += name1[0]
+    }
+    if (name2){
+        str += name2[0]
+    }
+    if (name3){
+        str += name3[0]
+    }
+    if(name4){
+        str += name4[0]
+    }
+    // str += name1[0];
+
+    // str = str + name1[0] + name2[0] + name3[0] + name4[0]
+
+    return str;
+
+}
+
+console.log(myMnemonic("Must", "Accept", "Sugar", "Happiness"))
+console.log(myMnemonic("We","Eat","Eggs"))
