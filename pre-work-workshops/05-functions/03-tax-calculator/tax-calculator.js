511cdb0a5f040e5946c30bbb37cec5f9cb7d4d29
sharmila657 <sharmilajirel8@gmail.com>
2023-06-05 15:01:56 +0545

    tax calculator

--- pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,6 +1,22 @@
 // YOUR CODE BELOW
- function taxCalculator(){
+
+ function taxCalculator(price,state){
+    if (state=== "NY"){
+        return price/100*4 + price 
+    }
+
+    else if(state === "NJ"){
+        return price/100*6.625 + price 
+    }
+   
+    // let aftertax = 4%
+    // let aftertax = 6.625%
+
+
+
 
  }
- taxCalculator(100, "NY");
- taxCalculator(100, "")
+ 
+
+ console.log(taxCalculator(50, "NY"))
+ console.log(taxCalculator(50, "NJ"))
