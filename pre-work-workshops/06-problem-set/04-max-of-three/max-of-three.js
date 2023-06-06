692993b0ea9604d2fc33c164c326e7745f7fc12a
sharmila657 <sharmilajirel8@gmail.com>
2023-06-06 08:03:24 +0545

    maxNumOfThree

--- pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
+++ pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+ function maxOfThree(n1, n2, n3){
+    let max_no;
+
+    if (n1 > n2){
+        max_no = n1;
+    }
+    else {
+        max_no = n2;
+    }
+    if (n3 > max_no){
+        max_no = n3
+    }
+    return max_no;
+
+
+ }
+ console.log(maxOfThree(5, 4, 10));
+ console.log(maxOfThree(7, 7, 4));
