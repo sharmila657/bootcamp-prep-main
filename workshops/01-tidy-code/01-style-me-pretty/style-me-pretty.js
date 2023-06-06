a92a4cee14cc714c70315706b542fc27eac4322f
sharmila657 <sharmilajirel8@gmail.com>
2023-06-06 14:15:37 +0545

    only-odds

--- workshops/01-tidy-code/01-style-me-pretty/style-me-pretty.js
+++ workshops/01-tidy-code/01-style-me-pretty/style-me-pretty.js
@@ -1,14 +1,24 @@
 // YOUR CODE BELOW
 function styleMePretty() {
-      let truth = '';
+  debugger
+  let truth = '';
 function innerBeauty(count) {
   let result = '';
+
   while(count > 0) {
     count--;
-      if(count===2) {
+      if(count === 2) {
       result += 'Unformatted code';
       }
-  } result += ' is difficult to parse';
-  return result;} truth += innerBeauty(5); truth += ' for humans if not for machines';
+
+  } 
+  result += ' is difficult to parse';
+  return result;
+} 
+
+truth += innerBeauty(5);
+truth += ' for humans if not for machines';
 return truth;
 }
+
+console.log(styleMePretty())
--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+
+function onlyOdds(num){
+    debugger
+    let sum = 0
+    for(let i=0; i<num; i++){
+    
+       if(i %2 !== 0){
+        sum += i
+       }
+       
+    }
+    return sum 
+   
+}
+
+console.log(onlyOdds(6));
