f49d44f62252eb150c4541d2d5378ce74b3ba5a7
sharmila657 <sharmilajirel8@gmail.com>
2023-06-06 16:52:08 +0545

    bacteria time

--- workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+
+function bacteriaTime(currentNum,targetNum){
+    debugger
+    
+    let time = 0
+    for(let i=currentNum; i < targetNum; i= i * 2 ){
+        
+            time += 20
+
+        }
+    return time
+   }
+console.log(bacteriaTime(1, 8));
