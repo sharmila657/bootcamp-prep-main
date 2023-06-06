bd66092bfdc0fb0a7ddc9a0c483a95eab0085510
sharmila657 <sharmilajirel8@gmail.com>
2023-06-06 08:30:54 +0545

    everyWhichWay

--- pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
+++ pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+
+function everyWhichWay(n1, n2, n3){
+    
+    if (n1 + n2 === n3){
+        return 'sum';
+    }
+    else if (n1 - n2 === n3){
+        return 'difference';
+    }
+    else if (n1 * n2 === n3){
+        return 'product';
+    }
+    else if(n1 / n2 === n3){
+        return 'fraction';
+    }
+    else {
+        return null;
+    }
+}
+console.log(everyWhichWay(10, 20, 30));
+console.log(everyWhichWay(50, 20, 30));
+console.log(everyWhichWay(4, 4, 16));
+console.log(everyWhichWay(100, 10, 10));
+console.log(everyWhichWay(1, 1000, 50));
