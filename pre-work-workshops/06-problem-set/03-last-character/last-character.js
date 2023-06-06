c0d1295615877688e4ecd056f81d96dd38fed1c1
sharmila657 <sharmilajirel8@gmail.com>
2023-06-06 07:33:45 +0545

    last character same

--- pre-work-workshops/06-problem-set/03-last-character/last-character.js
+++ pre-work-workshops/06-problem-set/03-last-character/last-character.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+
+function lastCharacter(name1,name2){
+    //last character of string
+    let lastchar1 = name1.at(-1)
+    let lastchar2 = name2.at(-1)
+
+//check two strings end with same character
+    if (lastchar1 === lastchar2 ){
+        return true;
+
+    }
+    else {
+        return false;
+    }
+}
+console.log(lastCharacter('apples','pumpkins'));
+console.log(lastCharacter('marker','pen'));
