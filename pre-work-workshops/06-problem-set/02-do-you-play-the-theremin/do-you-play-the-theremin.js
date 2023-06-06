f82694a617e4416869575ae0afee19a35fd6b5f6
sharmila657 <sharmilajirel8@gmail.com>
2023-06-06 06:59:51 +0545

    doYouPlayTheremin

--- pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+
+function doYouPlayTheTheremin(name){
+    if (name[0] === 's'|| name[0]==='S'){
+    return true;
+    }
+    else {
+        return false;
+    }
+
+}
+console.log(doYouPlayTheTheremin('Griffin'));
+console.log(doYouPlayTheTheremin('SCott'));
