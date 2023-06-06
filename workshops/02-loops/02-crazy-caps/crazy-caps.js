020e78487dddca79194f7e525b5073653390a9de
sharmila657 <sharmilajirel8@gmail.com>
2023-06-06 15:39:48 +0545

    crazyCaps

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+
+function crazyCaps(string){
+    debugger
+    let letters = ""
+    
+    for(let i = 0; i<string.length; i++){
+
+    if( i %2 !==0 ){
+     letters += string[i].toUpperCase()
+    }
+    else {
+    letters += string[i].toLowerCase()
+    }
+
+    }
+    
+    return letters
+    }
+
+    console.log(crazyCaps('fullstack is amazing'));
