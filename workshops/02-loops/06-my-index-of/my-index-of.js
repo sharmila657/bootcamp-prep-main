66e160c6305fe7254e0a767701cbcd8886caf059
sharmila657 <sharmilajirel8@gmail.com>
2023-06-07 08:07:11 +0545

    my index

--- workshops/02-loops/06-my-index-of/my-index-of.js
+++ workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+
+function myIndexOf(source, searchValue, startIdx) {
+    if (startIdx === undefined) {
+        startIdx = 0;
+    }
+    for (let i = startIdx; i < source.length; i++) {
+        if (source.slice(i, i + searchValue.length) === searchValue) {
+            return i;
+        }
+    }
+    return -1;
+    }
+    console.log(myIndexOf('hello', 'h')); 
+    console.log(myIndexOf('hello', 'e'));
+    console.log(myIndexOf('hello', 'l')); 
+    console.log(myIndexOf('hello', 'l')); 
+    console.log(myIndexOf('hello', 'o')); 
+    console.log(myIndexOf('twice twice', 'ice'));
+    console.log(myIndexOf('twice twice', 'ice', 5));
+    console.log(myIndexOf('happy string', 'sad'));
