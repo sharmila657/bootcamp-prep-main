2b4c09a7922a55ffbf414573dedc7e29ec4d7729
sharmila657 <sharmilajirel8@gmail.com>
2023-06-07 07:30:59 +0545

    bacteria time

--- workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,14 +1,20 @@
 // YOUR CODE BELOW
 
-function bacteriaTime(currentNum,targetNum){
+function bacteriaTime(currentNum, targetNum) {
     debugger
     
-    let time = 0
-    for(let i=currentNum; i < targetNum; i= i * 2 ){
+    let time = 0;
+    if (currentNum < targetNum) {
+        for (let i = currentNum; i < targetNum; i = i * 2) {
         
-            time += 20
+            time += 20;
 
         }
-    return time
-   }
+        return time;
+    }
+    else {
+        return "targetNum must be larger than currentNum";
+    }
+}
 console.log(bacteriaTime(1, 8));
+console.log(bacteriaTime(8,1))
