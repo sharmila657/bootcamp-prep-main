d51c375a08ccfb1b5357b2a1b18af8129e100b58
sharmila657 <sharmilajirel8@gmail.com>
2023-06-07 07:17:32 +0545

    exponentiate

--- workshops/02-loops/04-exponentiate/exponentiate.js
+++ workshops/02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function exponentiate(base, power) {
+    let result = 1;
+    for (let i = 0; i < power; i++){
+        result *= base;
+    }
+    return result;
+    
+}
+console.log(exponentiate(2, 2))
+console.log(exponentiate(3, 4))
