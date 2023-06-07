aeaf5066906ced2a6af626bb743452bac00eb467
sharmila657 <sharmilajirel8@gmail.com>
2023-06-07 13:55:11 +0545

    howEqual

--- workshops/02-loops/07-most-vowels/most-vowels.js
+++ workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function mostVowels(words) {
+    let maxVowelCount = 0;
+    let wordWithMaxVowels = "";
+//     const vowels = ["a", "e", "i", "o", "u"];
+
+//     const wordArray = words.split(" ");
+
+//     for (let word of word) {
+//         let maxVowelCount = 0;
+        
+//         for (let char of word) {
+//             if(vowels.includes(char))
+//         }
+//     }
+// }
--- workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
+++ workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+
+function howEqual(num1, num2) {
+
+    if (num1 === num2) {
+        return "strictly";
+    }
+    else if (num1 == num2) {
+        return "loosely";
+    }
+    else {
+        return "not equal"
+    }
+    
+}
+console.log(howEqual(0, "0"));
+console.log(howEqual(3, 9 / 3));
+console.log(howEqual(true, "truthy"));
