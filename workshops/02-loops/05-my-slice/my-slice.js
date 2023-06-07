42a75ece9fcd3c784015b4f4e3a26a59eb47bef0
sharmila657 <sharmilajirel8@gmail.com>
2023-06-07 07:49:04 +0545

    my slice

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+
+function mySlice(originalString, startIdx, endIdx) {
+    if (startIdx === undefined) {
+        startIdx = 0;
+        return originalString;
+    }
+    else if (endIdx === undefined) {
+        endIdx = originalString.length;
+    }
+    let slicedString = "";
+    
+    for (let i = startIdx; i < endIdx && i < originalString.length; i++){
+        slicedString += originalString[i];
+    }
+    return slicedString
+    
+}
+console.log(mySlice("slice and dice", 2));
+console.log(mySlice("slice and dice", 2, 5));
+console.log(mySlice("slice and dice"));
