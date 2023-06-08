688cc1cf9f78ff119b0b9f2689b9e1a68c051a65
sharmila657 <sharmilajirel8@gmail.com>
2023-06-08 07:32:50 +0545

    zero dark thirty

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+
+function zeroDarkThirty(num) {
+    let newNum = ""
+    if (num === 0) {
+        return NaN
+    }
+    else {
+        let convertNum = String(num);
+        for (let i = 0; i < convertNum.length; i++){
+            if (convertNum[i] !== "0") {
+                newNum += convertNum[i];
+            }
+        }
+        return Number(newNum);   
+        
+    }
+}
+console.log(zeroDarkThirty(102302));
+console.log(zeroDarkThirty(606.203));
+
+console.log(zeroDarkThirty(0))
