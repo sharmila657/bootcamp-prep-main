1282fbe66049a6ee8d42a2061c8847eee610f136
sharmila657 <sharmilajirel8@gmail.com>
2023-06-08 12:03:17 +0545

    solved increment

--- workshops/04-scope/02-incremental-change/incremental-change.js
+++ workshops/04-scope/02-incremental-change/incremental-change.js
@@ -1,3 +1,12 @@
 let counter = 0;
 
 // YOUR CODE BELOW
+function incrementalChange(num) {
+      
+    counter += num;
+    return counter
+  
+}
+console.log(incrementalChange(2));
+console.log(incrementalChange(6));
+console.log(incrementalChange(-1))
