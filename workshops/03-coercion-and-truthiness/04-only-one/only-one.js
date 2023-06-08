d21aa9c92c179c705257829f65463a5cdcb14590
sharmila657 <sharmilajirel8@gmail.com>
2023-06-08 07:16:57 +0545

    solved only aone

--- workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function onlyOne(ar1, ar2, ar3) {
+    if (!ar1 && !ar2 && ar3) {
+        return true
+    }
+    else if (!ar1 && ar2 && !ar3) {   //! (not false or not true)
+        return true
+    }
+    else if (ar1 && !ar2 && !ar3) {
+        return true
+    }
+    else {
+        return false
+    }
+    
+}
+console.log(onlyOne(false, false, true));
+console.log(onlyOne(0, 1, 2))
