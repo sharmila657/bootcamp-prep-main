1235696faad8be8b1f79e7e0423553478e9114d7
sharmila657 <sharmilajirel8@gmail.com>
2023-06-08 13:22:24 +0545

    solved sum things

--- workshops/04-scope/03-sum-things-wrong/sum-things-wrong.js
+++ workshops/04-scope/03-sum-things-wrong/sum-things-wrong.js
@@ -1,8 +1,12 @@
 let sum = 0;
 
 function sumThingsWrong(num1, num2) {
+  let sum = 0;
   sum += num1;
   sum += num2;
 
   return sum;
+  
 }
+console.log(sumThingsWrong(50, 100));
+// console.log(sumThingsWrong(50, 100));
