0598862e6260be9f4468cd629a77b7aa569ee371
sharmila657 <sharmilajirel8@gmail.com>
2023-06-09 15:47:41 +0545

    solved my reverse

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,17 +1,14 @@
 // // YOUR CODE BELOW
 function myReverse(array) {
     let reversedArray = [];
-    // for (let i = array.length - 1; i >= 0; i++){
-    
-
-
-
+    for (let i = array.length - 1; i >= 0; i++) {
     
         reversedArray.push(array[i]);
     }
     return reversedArray;
-
 }
+
+
 console.log(myReverse([1, 2, 3]));
 
 
