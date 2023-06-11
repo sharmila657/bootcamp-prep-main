fe966cee5918e813f6a4c48291b4f4f45443bbe1
sharmila657 <sharmilajirel8@gmail.com>
2023-06-11 23:53:11 +0545

    solved my unshift

--- workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -11,14 +11,15 @@
 
 function unShift(array, value) {
     let newArray = [value];
-    for (let element of array) {
+    for (let i = 0; i < array.length; i++) {
+        let element = array[i]
         newArray.push(element);
     
     }
     return newArray;
 }
 console.log(unShift([1, 2, 3], 0))
-Array.isArray
+
 
 
 
