3f18028e9e3d31fab7c79b40bb4d61c9a389231c
sharmila657 <sharmilajirel8@gmail.com>
2023-06-11 23:43:57 +0545

    solved my includes

--- workshops/05-arrays-i/02-my-includes/my-includes.js
+++ workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,21 +1,17 @@
 // // YOUR CODE BELOW
-// let arr = ""
-// function myIncludes() {
-//     console.log(myIncludes())
-//     arr.includes(arr)
-    
-// }
-// console.log(myIncludes([10, 20, 30],20))
 
 
 
 
 function myIncludes(array, searchValue) {
-    for (let element of array) {
+    debugger
+    for (let i = 0; i < array.length; i++) {
+        let element = array[i];
         if (element === searchValue) {
             return true;
 
         }
+        
         }
     return false;
     }
