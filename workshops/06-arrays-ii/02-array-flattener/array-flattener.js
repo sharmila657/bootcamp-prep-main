707e11a377b79aa56b4c048dd199ae917116a8c2
sharmila657 <sharmilajirel8@gmail.com>
2023-06-13 06:18:38 +0545

    solved array flatter

--- workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+
+
+function arrayFlattener(array) {
+    debugger
+    let flattenedArray = [];
+  
+    for (let i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i])) {
+        nestedArray = arrayFlattener(array[i]);
+        flattenedArray = flattenedArray.concat(nestedArray);
+      } else {
+        flattenedArray.push(array[i]);
+      }
+    }
+  
+    return flattenedArray;
+}
+console.log(arrayFlattener([1,[2, 3], 4])); 
+  
+  
