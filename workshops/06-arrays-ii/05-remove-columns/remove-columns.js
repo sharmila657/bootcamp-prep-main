567f8107463cf3ff7da08d9e6e4c14c2062c4c71
sharmila657 <sharmilajirel8@gmail.com>
2023-06-13 07:07:18 +0545

    solved my join

--- workshops/06-arrays-ii/05-remove-columns/remove-columns.js
+++ workshops/06-arrays-ii/05-remove-columns/remove-columns.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+function removeColumns(firstGrid, numColumns) {
+    debugger
+    let newGrid = [];
+
+    for (let i = 0; i < firstGrid.length; i++){
+        let newRow = [];
+        for (let j = 0; j < firstGrid[i].length; j++){
+            if (j >= numColumns) {
+                newRow.push(firstGrid[i][j]);
+            }
+        }
+        newGrid.push(newRow);
+
+    }
+    return newGrid;
+    
+}
+
+console.log(removeColumns([[1, 2, 3],
+[1, 2, 3],
+[1, 2, 3],
+[1, 2, 3]], 2));
--- workshops/06-arrays-ii/06-my-join/my-join.js
+++ workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+function myJoin(array, separator = ',') {
+    debugger
+    let result = '';
+  
+    for (let i = 0; i < array.length; i++) {
+      if (i !== 0) {
+        result += separator;
+      }
+  
+      if (array[i] === null || array[i] === undefined) {
+        result += '';
+      } else {
+        result += array[i];
+      }
+    }
+  
+    return result;
+  }
+console.log(myJoin(['let\'s', 'make', 'a', 'list'], ' '));
+console.log(myJoin(['a', 'b', 'c'], '+'));
+console.log(myJoin(['Peter', 'Paul', 'Mary']));
+console.log(myJoin(['hello', undefined, 'world'], '-'));
