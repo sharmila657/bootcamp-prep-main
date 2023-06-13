b57fe1d35d9b8e0fb3049144b205c19ba7e37549
sharmila657 <sharmilajirel8@gmail.com>
2023-06-13 06:40:17 +0545

    solved making grid

--- workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,5 +1,22 @@
 // YOUR CODE BELOW
 
-function zooInventory() {
+function zooInventory(zoo) {
+    let sentences = [];
+    for (let i = 0; i < zoo.length; i++){
+        let animal = zoo[i][0];
+        let species = zoo[i][1][0];
+        let age = zoo[i][1][1];
+
+        let sentence = "{animal} the {species} is {age}"
+        sentences.push(sentence);
+    }
+    return sentences;
     
 }
+let myZoo = [
+    ['King Kong', ['gorilla', 42]],
+    ['Nemo', ['fish', 5]],
+    ['Punxsutawney Phil', ['groundhog', 11]]
+  ];
+
+console.log(zooInventory(myZoo));
--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function makeGrid(Colums, Rows) {
+    debugger
+    let grid = [];
+
+    for (let i = 0; i < Rows; i++) {
+        let row = [];
+        
+        for (let j = 1; j <= Colums; j++){
+            row.push(j);
+        }
+        grid.push(row);
+        
+    }
+    return grid;
+}
+console.log(makeGrid(3, 4));
