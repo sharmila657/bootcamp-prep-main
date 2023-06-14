63992ba52fdbeab97f34c70c244e71841d4c19e9
sharmila657 <sharmilajirel8@gmail.com>
2023-06-14 06:55:01 +0545

    solved my slice

--- workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -3,49 +3,34 @@ function mySlice(originalArray, startIdx, endIdx) {
     let newArray = [];
 
     
-    // console.log(originalArray)
-   //     let copyArray = [];
-    //     for (let i = startIdx; i < endIdx; i++){
-    //         copyArray.push(originalArray[i])
-
-    if (startIdx === undefined && endIdx === undefined) {
-        return originalArray;
+   if (startIdx === undefined) {
+        return originalArray.slice();
     
     }
 
-    if (startIdx !== undefined && endIdx === undefined) {
-        for (let i = startIdx; i < originalArray.length; i++) {
-            newArray.push(originalArray[i])
-        }
-    }
-    if (startIdx !== undefined && endIdx !== undefined) {
-        for (i = startIdx; i < endIdx; i++) {
-            newArray.push(originalArray[i])
-        
-        }
-    }
-    let end = originalArray.length - startIdx;
-
-    if (startIdx < 0 && endIdx === undefined) {
-        for (i = 0; i <= end; i++) {
-            newArray.push(originalArray[i]);
-        }
-    }
-      if (startIdx < 0 && endIdx === undefined){
-       for (let i = 0 + 1; i <= endIdx; i++){
-            newArray.push(originalArray[i])
-        }
-    }
+    // if (startIdx !== undefined && endIdx === undefined) {
     
-    // if (startIdx < 0 && endIdx === undefined) {
-    //     newArray = originalArray.length + endIdx;
-    //   }
+    if (startIdx < 0) {
+        startIdx = originalArray.length + startIdx;
+    }
+
+    if (endIdx === undefined || endIdx > originalArray.length) {
+        endIdx = originalArray.length;
+    }
+    else if (endIdx < 0) {
+        endIdx = originalArray.length + endIdx;
+    }
+    for (let i = startIdx; i < endIdx; i++){
+            newArray.push(originalArray[i])
+    }
     return newArray;
-}
+    }
+    
 
-// console.log(mySlice([1, 2, 3], 1, 2));
-// console.log(mySlice([1, 2, 3], 1));
-// console.log(mySlice([1, 2, 3]));
+
+console.log(mySlice([1, 2, 3], 1, 2));
+console.log(mySlice([1, 2, 3], 1));
+console.log(mySlice([1, 2, 3]));
 console.log(mySlice([1, 2, 3],-1));
 
 
