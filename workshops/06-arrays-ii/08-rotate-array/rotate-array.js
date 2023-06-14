15d9ac744b8411431eb996cd04cb5256746580f0
sharmila657 <sharmilajirel8@gmail.com>
2023-06-14 06:33:24 +0545

    solved rotate

--- workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+function rotateArray(originalArray, rotateNum) {
+    newArray = originalArray.slice();
+
+    if (rotateNum > 0) {
+        for (let i = 0; i < rotateNum; i++){
+            lastElement = newArray.pop();
+            newArray.unshift(lastElement);
+        }
+    }
+    else if (rotateNum < 0) {
+        for (let i = 0; i > rotateNum; i--){
+            firstElement = newArray.shift();
+            newArray.push(firstElement);
+        }
+    }
+ 
+    return newArray; 
+}
+console.log(rotateArray([1, 2, 3, 4, 5], 1));
+console.log(rotateArray([1, 2, 3, 4, 5], -1));
+console.log(rotateArray([1, 2, 3, 4, 5], 0));
