4e28f581ef8114d5ff128486ed7d7f361fa62af9
sharmila657 <sharmilajirel8@gmail.com>
2023-06-14 07:27:50 +0545

    compare objects solved

--- workshops/07-objects/02-compare-objects/compare-objects.js
+++ workshops/07-objects/02-compare-objects/compare-objects.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+
+function compareObjects(obj1, obj2) {
+    let keys1 = Object.keys(obj1);
+    let keys2 = Object.keys(obj2);
+
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+    for (let key of keys1) {
+        if (obj1[key] !== obj2[key]) {
+            return false;
+        }
+        
+    }
+    return true;
+}
+console.log(compareObjects({ name: 'nick' }, { name: 'nick' }));
+console.log(compareObjects({ name: 'zeke' }, { name: 'zeke', age: 12 }));
