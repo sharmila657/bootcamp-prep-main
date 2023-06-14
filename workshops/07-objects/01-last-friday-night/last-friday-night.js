4fa463ab69b095b75d2bb731fdb29ed313da060b
sharmila657 <sharmilajirel8@gmail.com>
2023-06-14 07:05:40 +0545

    solved last friday night

--- workshops/07-objects/01-last-friday-night/last-friday-night.js
+++ workshops/07-objects/01-last-friday-night/last-friday-night.js
@@ -1,1 +1,28 @@
 // YOUR CODE BELOW
+function lastFridayNight(transactions) {
+    let totalAmount = 0;
+    for (let i = 0; i < transactions.length; i++){
+        totalAmount += transactions[i].amount;
+    }
+    return totalAmount;
+}
+let transactions = [
+    {
+      name: "Tons of glitter",
+      amount: 70
+    },
+    {
+      name: "Porcelain Pink Flamingos",
+      amount: 92
+    },
+    {
+      name: "Chandelier replacement",
+      amount: 10000,
+    },
+    {
+      name: "Dinner at TGIF x6",
+      amount: 350
+    }
+];
+  
+console.log(lastFridayNight(transactions));
