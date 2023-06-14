004db01a7bc1d9082064a6180f85ec28c271e721
sharmila657 <sharmilajirel8@gmail.com>
2023-06-14 16:16:48 +0545

    solved call them all

--- workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,1 +1,27 @@
 // YOUR CODE BELOW
+
+function callThemAll(object, value) {
+    let val = [];
+    for (let key in object) {
+        // console.log(key)
+        // console.log([key])
+        if (typeof object[key] === "function") {
+            val.push(object[key](value));
+        }
+    }
+    return val;
+}
+
+let addsNums = {
+    addTen: function(num) {
+      return num + 10;
+    },
+  
+    addTwenty: function(num) {
+      return num + 20;
+    },
+  
+    someProperty: 'value'
+  }
+  
+console.log(callThemAll(addsNums, 100));
