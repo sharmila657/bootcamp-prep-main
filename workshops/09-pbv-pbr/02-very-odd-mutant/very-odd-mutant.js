7cb86bbe000d019e2ee672c29eedb7c212fa9d00
sharmila657 <sharmilajirel8@gmail.com>
2023-06-15 15:55:39 +0545

    very odd mutant

--- workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
+++ workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
@@ -1,1 +1,27 @@
 // YOUR CODE BELOW
+
+function veryOddMutant(allTheNums) {
+    let countRemoved = 0;
+    for (i = 0; i < allTheNums.length; i++){
+      
+        if (allTheNums[i] % 2 === 0) {
+            allTheNums[i] = 'normie';
+            countRemoved++
+        }
+        
+    }
+
+    return countRemoved;
+    
+    
+}
+
+
+let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
+let countRemoved = veryOddMutant(allTheNums);
+
+console.log('allTheNums:', allTheNums);
+// [1, 'normie', 3, 'normie', 5, 'normie', 7, 'normie'];
+
+console.log('countRemoved:', countRemoved);
+// 4
