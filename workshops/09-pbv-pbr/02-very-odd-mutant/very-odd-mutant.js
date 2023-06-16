ed7de30fcf0bf781011549a02522d46e8de7494c
sharmila657 <sharmilajirel8@gmail.com>
2023-06-16 07:55:31 +0545

    solved clone machine

--- workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
+++ workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
@@ -1,27 +1,52 @@
 // YOUR CODE BELOW
 
-function veryOddMutant(allTheNums) {
-    let countRemoved = 0;
-    for (i = 0; i < allTheNums.length; i++){
+// function veryOddMutant(allTheNums) {
+//     // let countRemoved = 0;
+//     // for (i = 0; i < allTheNums.length; i++){
       
-        if (allTheNums[i] % 2 === 0) {
-            allTheNums[i] = 'normie';
-            countRemoved++
-        }
+//     //     if (allTheNums[i] % 2 === 0) {
+//     //         allTheNums[i] = 'normie';
+//     //         countRemoved++
+//     //     }
         
+//     // }
+
+//     // return countRemoved;
+    
+    
+// }
+
+
+function veryOddMutant(numbers) {
+    let countRemoved = 0;
+  
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] % 2 === 0) {
+        numbers[i] = 'normie';
+        countRemoved++;
+      }
     }
-
+  
     return countRemoved;
-    
-    
-}
+  }
+  
+  // Test the function
+  let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
+  let countRemoved = veryOddMutant(allTheNums);
+  
+  console.log('allTheNums:', allTheNums);
+  // [1, 'normie', 3, 'normie', 5, 'normie', 7, 'normie']
+  
+  console.log('countRemoved:', countRemoved);
+  // 4
+  
 
 
-let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
-let countRemoved = veryOddMutant(allTheNums);
+// let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
+// let countRemoved = veryOddMutant(allTheNums);
 
-console.log('allTheNums:', allTheNums);
-// [1, 'normie', 3, 'normie', 5, 'normie', 7, 'normie'];
+// console.log('allTheNums:', allTheNums);
+// // [1, 'normie', 3, 'normie', 5, 'normie', 7, 'normie'];
 
-console.log('countRemoved:', countRemoved);
-// 4
+// console.log('countRemoved:', countRemoved);
+// // 4
--- workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
+++ workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+debugger
+function cloneMachine(animal) {
+    let clone = {
+        name: animal.name + 'Clone',
+        species: animal.species,
+        offspring: []
+    };
+    animal.offspring.push(clone.name);
+
+    return clone;
+    
+}
+
+let dolly = {
+    name: 'Dolly',
+    species: 'sheep',
+    offspring: []
+  }
+  
+let dollyClone = cloneMachine(dolly);
+
+console.log(dollyClone)   
+console.log(dolly) 
