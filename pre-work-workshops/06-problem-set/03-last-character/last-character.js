039623770486480000176445e3c934c1146ff8b1
sharmila657 <sharmilajirel8@gmail.com>
2023-06-16 15:38:53 +0545

    solved last character

--- pre-work-workshops/06-problem-set/03-last-character/last-character.js
+++ pre-work-workshops/06-problem-set/03-last-character/last-character.js
@@ -1,11 +1,13 @@
 // YOUR CODE BELOW
 
 function lastCharacter(name1,name2){
-    //last character of string
-    let lastchar1 = name1.at(-1)
+    
+    let lastchar1 = name1.at(1)
+    // console.log(lastchar1)
     let lastchar2 = name2.at(-1)
+    console.log
 
-//check two strings end with same character
+
     if (lastchar1 === lastchar2 ){
         return true;
 
