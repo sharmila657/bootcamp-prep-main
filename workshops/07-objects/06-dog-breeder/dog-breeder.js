033c6785cafc4bec8256aa5be234f0ac83b1fc12
sharmila657 <sharmilajirel8@gmail.com>
2023-06-18 22:21:54 +0545

    solved dog breeder

--- workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+debugger
+function dogBreeder(name, age) {
+    let object = {
+
+    }
+    if (typeof name === "number") {
+        object["name"] = "Steve"
+        object["age"]= name
+        
+    }
+    else {
+        object.name = name || "Steve"
+        object.age = age || 0
+    }
+    
+    return object;
+
+}
+console.log(dogBreeder(15));
+console.log(dogBreeder("Sam", 15));
+
