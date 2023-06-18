d499374fb7fadfdaf0755891643541a2affefef7
sharmila657 <sharmilajirel8@gmail.com>
2023-06-19 05:43:22 +0545

    frequency analysis

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,15 @@
-// YOUR CODE BELOW
+
+function frequencyAnalysis(stringVal) {
+    let finalObj = {}
+    for (let k = 0; k < stringVal.length; k++) {
+        if (stringVal[k] in finalObj) {
+            finalObj[stringVal[k]] = finalObj[stringVal[k]] + 1
+        } else {
+            finalObj[stringVal[k]] = 1
+        }
+    }
+    console.log(finalObj)
+    return finalObj
+}
+
+frequencyAnalysis('abca')
