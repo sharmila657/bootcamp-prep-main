9245b9d3119c09cd698b95ca6f9663eb7b6713f2
sharmila657 <sharmilajirel8@gmail.com>
2023-06-19 06:41:13 +0545

    solved my for each

--- workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
+++ workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function myForEach(array, myFunc) {
+    for (i = 0; i < array.length; i++){
+        myFunc(array[i],i)
+    }
+}
+let sum = 0;
+
+function addToSum(num) {
+    sum += num;
+}
+
+let nums = [1, 2, 3];
+
+myForEach(nums, addToSum);
+
+console.log(sum);
