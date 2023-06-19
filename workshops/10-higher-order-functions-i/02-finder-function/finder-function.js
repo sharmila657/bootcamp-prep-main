d97d92b21d6cd5dd955531eaf06a7d5cc84e074a
sharmila657 <sharmilajirel8@gmail.com>
2023-06-19 06:23:41 +0545

    finder function

--- workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
+++ workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+
+function finderFunction(array,myFunc){
+    for(let i=0;i<array.length;i++){
+        if (myFunc(array[i])){
+            return i;
+        } 
+     }
+    return -1
+}
+let numbers = [1, 3, 5, 64, 7, 12];
+let odds = [9, 13, 15, 17];
+
+function isEven(num) {
+  return !(num % 2);
+}
+
+
+
+
+console.log(finderFunction(numbers, isEven))
+console.log(finderFunction(odds, isEven))
