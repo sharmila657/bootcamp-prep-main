dbc96bbc22a96b87fd4cd8ae8a07da9822f6d67c
sharmila657 <sharmilajirel8@gmail.com>
2023-06-19 15:09:04 +0545

    solved call count

--- workshops/11-higher-order-functions-ii/04-call-count/call-count.js
+++ workshops/11-higher-order-functions-ii/04-call-count/call-count.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+debugger
+
+function callCount() {
+    let count = 0;
+
+    return function(){
+        count++;
+        return count;
+    }
+}
+
+let newFunction1 = callCount();
+let newFunction2 = callCount();
+
+console.log(newFunction1());
+console.log(newFunction1());
+
+
+console.log(newFunction2());
+console.log(newFunction2())
