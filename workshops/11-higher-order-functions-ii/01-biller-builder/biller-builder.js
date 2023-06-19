7803d2528449a2ab75444e8b967d008905f68f3f
sharmila657 <sharmilajirel8@gmail.com>
2023-06-19 14:25:29 +0545

    solved biller builder

--- workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
+++ workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
@@ -1,1 +1,34 @@
 // YOUR CODE BELOW
+
+debugger
+
+function billerBuilder(state) {
+    if (state === 'NY') {
+        return function (price) {
+            let shippingCost = price * 0.03;
+            let tax = (price + shippingCost) * 0.04;
+            let finalPrice = price + shippingCost + tax;
+            return finalPrice;
+        }
+    }
+    else if (state === 'NJ') {
+        return function (price) {
+            let shippingCost = price * 0.05;
+            let tax = (price + shippingCost) * 0.06625;
+            let finalPrice = price + shippingCost + tax;
+            return finalPrice;
+        }
+    }
+    else {
+        return function (price) {
+            return price;
+        }
+    }
+}
+
+
+let newYorkBiller = billerBuilder('NY');
+console.log(newYorkBiller(100));
+
+let newJersBiller = billerBuilder('NJ')
+console.log(newJersBiller(100));
