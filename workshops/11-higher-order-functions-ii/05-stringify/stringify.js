0a6e805101c091d8847fc4c85ca6938efdbc9b20
sharmila657 <sharmilajirel8@gmail.com>
2023-06-19 16:23:52 +0545

    solved stringify

--- workshops/11-higher-order-functions-ii/05-stringify/stringify.js
+++ workshops/11-higher-order-functions-ii/05-stringify/stringify.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+
+function stringify(callback) {
+    return function () {
+        let myFunc = callback();
+        return myFunc.toString();
+
+    }
+        
+}
+
+
+function returnsANumber() {
+    return 100;
+}
+
+let newFunction = stringify(returnsANumber);
+console.log(newFunction());
