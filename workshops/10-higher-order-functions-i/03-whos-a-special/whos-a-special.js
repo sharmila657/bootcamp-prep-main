765a444ff1e3a52b540564fe1e879e59d56c7180
sharmila657 <sharmilajirel8@gmail.com>
2023-06-19 06:23:26 +0545

    solved who is special

--- workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,1 +1,31 @@
 // YOUR CODE BELOW
+function whosASpecial (array) {
+    let arr=[]
+    array.forEach(function(para){
+        let name=para.name
+        let species=para.species
+        let stringName=name+" the "+species+" is very special!"
+        arr.push(stringName)
+
+    })
+    let finalString=arr.join(" ")
+console.log(finalString)
+ return finalString
+}
+
+
+
+
+
+let specialPets = [{
+    name: 'Sadie',
+    species: 'cat'
+  }, {
+    name: 'Layla',
+    species: 'cat'
+  }, {
+    name: 'Bogie',
+    species: 'dog'
+  }
+];
+whosASpecial(specialPets)
