ec975d58f222d5f06881de8bbef1c1765a6716d5
sharmila657 <sharmilajirel8@gmail.com>
2023-06-19 07:36:22 +0545

    solved most vowels

--- workshops/02-loops/07-most-vowels/most-vowels.js
+++ workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,16 +1,40 @@
-// YOUR CODE BELOW
-function mostVowels(words) {
-    let maxVowelCount = 0;
-    let wordWithMaxVowels = "";
-//     const vowels = ["a", "e", "i", "o", "u"];
+// // YOUR CODE BELOW
 
-//     const wordArray = words.split(" ");
 
-//     for (let word of word) {
-//         let maxVowelCount = 0;
-        
-//         for (let char of word) {
-//             if(vowels.includes(char))
-//         }
-//     }
-// }
+debugger
+function mostVowels(arg1) {
+    let currentvowel = 0;
+    let highestvowel = 0;
+    let currentletter = "";
+    let highestvowelletter = ""; 
+    for (let i = 0; i < arg1.length; i++){
+        if (arg1[i] === "a" || arg1[i] === "e" || arg1[i] === "i" || arg1[i] === "o"|| arg1[i] === "u"|| arg1[i] === "I") {
+            currentvowel++
+        }
+        if (arg1[i] === " ") {
+            if (currentvowel > highestvowel) {
+                highestvowel = currentvowel
+                highestvowelletter = currentletter
+            }
+            currentletter = ""
+            currentvowel = 0;
+
+
+            
+        }
+        else {
+            currentletter += arg1[i]
+        }
+
+
+    }
+
+    return highestvowelletter;
+    
+}
+console.log(mostVowels('gi fe peev '));
+console.log(mostVowels("Peeves"));
+console.log(mostVowels('I am a keeper with some real rhythms'));
+
+
+
