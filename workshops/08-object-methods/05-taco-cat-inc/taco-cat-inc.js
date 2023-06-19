f5b8fa1b45c367b48e705f997cc52ddefd0bea4e
sharmila657 <sharmilajirel8@gmail.com>
2023-06-19 05:55:28 +0545

    solved taco cat

--- workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.js
+++ workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.js
@@ -19,13 +19,42 @@ let tacoCatInc = {
     'treat dust': {cost: 0.1, quantity: 100}
   },
 
-  cash: 0
-};
+  cash: 0,
+
 
 // YOUR CODE BELOW
 
-let object = {
-  currentInventory:function(){
-    
+currentInventory: function () {
+  let total_value = 0
+  for (keys in tacoCatInc) {
+      for (keys1 in tacoCatInc[keys]) {
+          let tempVal = tacoCatInc[keys][keys1]
+          console.log(tempVal, tempVal['cost'], tempVal['quantity'])
+          let individual_item = tempVal['cost'] * tempVal['quantity']
+          total_value = total_value + individual_item
+      }
   }
+  if (this.cash !== 0) {
+    return total_value
+  }
+  total_value = total_value + this.cash
+  return total_value
+},
+sale: function (order) {
+  let finalcost = 0
+  for (keys in order) {
+      tacoCatInc[keys][order[keys]]['quantity']--
+      let items = tacoCatInc[keys][order[keys]]
+      finalcost = finalcost + items['cost']
+  }
+  this.cash = this.cash + finalcost
+  return finalcost
+},
 }
+
+let order = {
+  gourmetShell: 'hard treat shell',
+  gourmetFishFilling: 'sardines',
+  gourmetSeasoning: 'treat dust',
+}
+console.log(tacoCatInc.currentInventory())
