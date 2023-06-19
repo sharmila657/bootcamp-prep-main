948e9a67bf3936b56a925da5fe95d0ec1d3bab0e
sharmila657 <sharmilajirel8@gmail.com>
2023-06-19 06:23:54 +0545

    logger

--- workshops/10-higher-order-functions-i/01-function-logger/function-logger.js
+++ workshops/10-higher-order-functions-i/01-function-logger/function-logger.js
@@ -1,28 +1,16 @@
 // YOUR CODE BELOW
 debugger
-function functionLogger(squareNum, arg) {
-    let newfunc = "Function starting";
-    console.log("Function complete");
-    return newfunc;
 
+
+function functionLogger(squareNum,num){
+    return  squareNum(num);
 }
 
-function addsTen(num) {
-
-//     // for (let i = 0;)
-    return num + 10;
-
- 
+function squareNum(num){
+    return num*num;
 }
 
-
-function squareNum(x) {
-
-
-    return x * x;
-    
-}
-
-// let squareNum = 4;
-let squareOfFour = functionLogger(squareNum, 4);
+let squareOfFour =functionLogger(squareNum,4);
 console.log(squareOfFour)
+
+
