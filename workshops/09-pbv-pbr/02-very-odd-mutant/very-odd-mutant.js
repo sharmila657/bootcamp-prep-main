3d91219ff6050d8628e5ddb37e04423ed3f63d30
sharmila657 <sharmilajirel8@gmail.com>
2023-06-19 06:06:47 +0545

    solved very odd mutant

--- workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
+++ workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
@@ -1,22 +1,20 @@
 // YOUR CODE BELOW
 
-function veryOddMutant(numbers) {
-    let countRemoved = 0;
-  
-    for (let i = 0; i < numbers.length; i++) {
-      if (numbers[i] % 2 === 0) {
-        numbers[i] = 'normie';
-        countRemoved++;
+function veryOddMutant(allTheNums){
+  let count=0
+
+  for(let i=0;i<allTheNums.length;i++){
+      if(allTheNums[i]%2===0){
+          allTheNums[i]="normie"
+          count++
       }
-    }
-  
-    return countRemoved;
   }
+  return count;
+}
 
+// let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
+// let countRemoved = veryOddMutant(allTheNums);
 
-let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
-let countRemoved = veryOddMutant(allTheNums);
+// console.log('allTheNums:', allTheNums);
 
-console.log('allTheNums:', allTheNums);
-
-console.log('countRemoved:', countRemoved);
+// console.log('countRemoved:', countRemoved);
