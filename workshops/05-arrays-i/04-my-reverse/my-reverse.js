0d129ea01264b65af39a3e78f4d39b2030918934
sharmila657 <sharmilajirel8@gmail.com>
2023-06-22 13:42:41 +0545

    my reverse

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,7 +1,9 @@
 // // YOUR CODE BELOW
+debugger
 function myReverse(array) {
     let reversedArray = [];
-    for (let i = array.length - 1; i >= 0; i++) {
+    for (let i = (array.length)-1; i >= 0; i++) {
+    // for(let i=array.length)
     
         reversedArray.push(array[i]);
     }
