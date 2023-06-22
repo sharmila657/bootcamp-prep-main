321add3097da5d8848c2633077477b8fbecc5a07
sharmila657 <sharmilajirel8@gmail.com>
2023-06-22 16:25:14 +0545

    solved zero dark thirty

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,22 +1,26 @@
 // YOUR CODE BELOW
 
 function zeroDarkThirty(num) {
+    debugger
     let newNum = ""
     if (num === 0) {
         return NaN
     }
+
     else {
         let convertNum = String(num);
-        for (let i = 0; i < convertNum.length; i++){
+        for (let i = 0; i < convertNum.length; i++) {
             if (convertNum[i] !== "0") {
                 newNum += convertNum[i];
             }
         }
-        return Number(newNum);   
-        
+        return Number(newNum);
     }
 }
+
 console.log(zeroDarkThirty(102302));
 console.log(zeroDarkThirty(606.203));
-
 console.log(zeroDarkThirty(0))
+
+
+
