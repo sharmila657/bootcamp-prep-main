fdf23100a37a88914377644674a99d068e55237c
sharmila657 <sharmilajirel8@gmail.com>
2023-06-22 13:42:55 +0545

    my unshift

--- workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,13 +1,5 @@
 // YOUR CODE BELOW
-
-// function unShift(array, value) {
-//     let newArray = [value];
-//     for (let i = 0; i < array.length; i++) {
-//         newArray.push(value);
-//     }
-//     return newArray;   
-// }
-// console.log(unShift([1, 2, 3], 0));
+debugger
 
 function unShift(array, value) {
     let newArray = [value];
