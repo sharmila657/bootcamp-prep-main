13b8486a7bd4e18db5d0618ccfb129735ff0ef95
sharmila657 <sharmilajirel8@gmail.com>
2023-06-22 13:43:24 +0545

    get dog breeder

--- workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
+++ workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
@@ -1,1 +1,28 @@
 // YOUR CODE BELOW
+function getDogBreeder(defaultName = "steve", defaultAge = 0) {
+    return function getDogBreeder(newName, newAge) {
+        if (!(newName || newAge)) {
+            return{name:defaultName, age:defaultAge}
+        }
+        else if (typeof newName === "number") {
+            return{name:defaultName, age:newName}
+        }
+        else if (!newAge) {
+            return{name:newName, age:defaultAge}
+        }
+        else {
+            return {
+                name:newName, age:newAge
+            }
+        }
+    }
+}
+
+
+let puppyFarm = getDogBreeder('Snoopy', 0);
+let rescueShelter = getDogBreeder('Odie', 3);
+
+console.log(puppyFarm('Olaf', 3));
+console.log(puppyFarm(2));
+
+console.log(rescueShelter());
