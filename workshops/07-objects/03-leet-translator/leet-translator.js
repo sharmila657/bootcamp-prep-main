c84f0e8173c0de0a2291ea3cf88c52aa29a5099f
sharmila657 <sharmilajirel8@gmail.com>
2023-06-26 06:03:08 +0545

    not finished

--- workshops/07-objects/03-leet-translator/leet-translator.js
+++ workshops/07-objects/03-leet-translator/leet-translator.js
@@ -2,11 +2,57 @@ let letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 let leetChars = ['@', '8', '(', '|)', '3', 'ph', 'g', '#','l', '_|', '|<', '1', "|'|'|", '/\/', '0', '|D', '(,)', '|2', '5', '+', '|_|', '|/', "|/|/'",'><', 'j', '2'];
 
 // YOUR CODE BELOW
-function leetTranslator(string) {
+// function leetTranslator(char) {
+//     let newChar = [];
+//     let value = [];
+//     for (let i = 0; i < letters.length; i++){
+//         if (newChar[i] === letters[i]) {
+//             value = i;
+//         }
+//     }
+//     for (let i = 0; i < leetChars.length; i++){
+//         if (value[i] === leetChars[i]) {
+//             newChar = leetChars[i];
+//         }
+
+//     }
+//     for (let i = 0; i < char.length; i++){
+//         let  = char[i].toLowerCase();
+//     }
+//     return newChar.join();
     
+
+// }
+// // console.log(leetTranslator("Sharmila"))
+
+
+// console.log(leetTranslator('Fullstack') )
+
+function leetTranslator(objectChar)
+{
+   let newWord = [];
+   let value = [];
+   for(let i=0; i<letters.length; i++)
+   {
+        if(newWord[i] === letters[i])
+        {
+            value = i;
+        }
+   }
+   for(let i=0; i<leetChars.length; i++)
+   {
+        if(value[i] === leetChars[i])
+        {
+            newWord = leetChars[i];
+        }
+   }
+   for(let i=0; i<objectChar.length; i++)
+   {
+    let l = objectChar[i]. toLowerCase();
+   }
+   return newWord.join();
 }
-
-
+leetTranslator("Sharmila");
 
 
 
